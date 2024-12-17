74ae2cc49428e8d1b882724da8ceaaa915d93263
Kim-Jong-Yun <rlawhddbs012@naver.com>
2024-12-17 14:48:34 +0900

    ㅁㅁㅁ

--- robot_server/src/routes/robot.route.js
+++ robot_server/src/routes/robot.route.js
@@ -4,30 +4,33 @@ const robotController = require('../controllers/robot.controller');
 const workflowQueueController = require('../controllers/workflowQueue.controller');
 const auth = require('../middlewares/auth').authenticate;
 
+// 로봇 관련 라우트
 router.get('/robots', auth, robotController.getRobots);
 router.post('/register', auth, robotController.registerRobot);
 router.put('/update/:id', auth, robotController.updateRobot);
 router.post('/send_map', auth, robotController.sendMapToRobots);
-/*router.put('/unregister/:id', auth, robotController.unregisterRobot);*/
 router.delete('/robots/:id', auth, robotController.unregisterRobot);
 router.put('/updatePosition', robotController.updateRobotPosition);
 router.post('/bringup/:robotIp', auth, robotController.bringupRobot);
 router.put('/battery/update', robotController.updateRobotBattery);
 router.put('/update_status', robotController.updateRobotStatus);
-/*router.put('/update_speed', robotController.updateRobotSpeed);*/
 router.put('/toggleActive/:id', auth, robotController.toggleRobotActive);
 router.get('/robots/active', auth, robotController.getActiveRobots);
 
+// 워크플로우 큐 관련 라우트
 router.post('/addWorkflow', workflowQueueController.addWorkflowToRobotQueue);
 router.get('/robot/:robotId', workflowQueueController.getWorkflowQueueByRobotId);
 router.delete('/:robotId/workflows/:workflowId', workflowQueueController.removeWorkflowByIdFromRobotQueue);
 router.post('/assignNextWorkflow', auth, robotController.assignNextWorkflow);
 
-
+// ROS 관련 라우트 주석 처리
+/*
 router.post('/rosbridge/start', robotController.startRosbridgeWebsocket);
-router.post('/rosbridge/stop',  robotController.stopRosbridgeWebsocket);
-
+router.post('/rosbridge/stop', robotController.stopRosbridgeWebsocket);
 router.post('/move-to-task', auth, robotController.moveRobotToTask);
+*/
+
+// 현재 워크플로우 초기화 라우트
 router.put('/robots/:id/clear-current-workflow', robotController.clearCurrentWorkflow);
 
 module.exports = router;
